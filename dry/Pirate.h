29111dc2a49071beb09c929e321027e54307e86f
zahwaazbarka <zahwaazbarka@gmail.com>
2025-06-05 17:03:11 +0300

    Update Pirate.h

--- dry/Pirate.h
+++ dry/Pirate.h
@@ -5,18 +5,35 @@
 
 using std::string;
 
+enum DEVIL_FRUIT {
+    GUM_GUM_FRUIT,
+    SMOKE_SMOKE_FRUIT,
+    FLAME_FLAME_FRUIT,
+    RUMBLE_RUMBLE_FRUIT,
+    STRING_STRING_FRUIT,
+    ICE_ICE_FRUIT,
+    NONE
+};
+
 class Pirate {
 private:
     string name;
-
+    int bounty;
 public:
+    DEVIL_FRUIT devilFruit;
     Pirate(const string& name);
+    Pirate();
+    Pirate(const string& name, DEVIL_FRUIT devilFruit = DEVIL_FRUIT::NONE);
+    Pirate(const string& name,const int& bounty ,DEVIL_FRUIT devilFruit = DEVIL_FRUIT::NONE);
+
 
     ~Pirate() = default;
-
     void setName(const string& name);
-
+    void setBounty(const int& bounty);
+    int getBounty();
+    void setDevilFruit(DEVIL_FRUIT devilFruit);
     string getName();
+    DEVIL_FRUIT getDevilFruit();
 
     friend std::ostream &operator<<(std::ostream &os, const Pirate &pirate);
 };
